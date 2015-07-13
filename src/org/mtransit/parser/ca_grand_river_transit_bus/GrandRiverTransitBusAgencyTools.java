4a5742f70b1520139cb80b12a3fda2b9fa3bc76b
Mathieu Méa <mathieu.mea@gmail.com>
2015-07-13 16:55:44 -0400

    Minor improvements...

--- src/org/mtransit/parser/ca_grand_river_transit_bus/GrandRiverTransitBusAgencyTools.java
+++ src/org/mtransit/parser/ca_grand_river_transit_bus/GrandRiverTransitBusAgencyTools.java
@@ -38,7 +38,7 @@ public class GrandRiverTransitBusAgencyTools extends DefaultAgencyTools {
 
 	@Override
 	public void start(String[] args) {
-		System.out.printf("\nGenerating Grand River Transit bus data...\n");
+		System.out.printf("\nGenerating Grand River Transit bus data...");
 		long start = System.currentTimeMillis();
 		this.serviceIds = extractUsefulServiceIds(args, this);
 		super.start(args);
