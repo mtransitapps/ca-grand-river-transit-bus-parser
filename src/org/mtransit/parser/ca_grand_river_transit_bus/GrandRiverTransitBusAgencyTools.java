6d8d0b6ed0cb5b08a9d3ce3a6ed95ddcf37b997f
Mathieu Méa <mathieu.mea@gmail.com>
2018-07-16 10:47:03 -0400

    Compatible with Current/Next GTFS data

--- src/org/mtransit/parser/ca_grand_river_transit_bus/GrandRiverTransitBusAgencyTools.java
+++ src/org/mtransit/parser/ca_grand_river_transit_bus/GrandRiverTransitBusAgencyTools.java
@@ -59,6 +59,11 @@ public class GrandRiverTransitBusAgencyTools extends DefaultAgencyTools {
 	}
 
 	@Override
+	public boolean excludingAll() {
+		return this.serviceIds != null && this.serviceIds.isEmpty();
+	}
+
+	@Override
 	public boolean excludeCalendar(GCalendar gCalendar) {
 		if (this.serviceIds != null) {
 			return excludeUselessCalendar(gCalendar, this.serviceIds);
